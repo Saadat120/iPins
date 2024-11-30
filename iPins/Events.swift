2d932cfc6c8a09b4a8f9adf224e8154eaa75d5dc
Saadat120 <90794702+Saadat120@users.noreply.github.com>
2024-11-30 16:24:02 -0500

    Update Events.swift

--- iPins/Events.swift
+++ iPins/Events.swift
@@ -5,15 +5,19 @@
 //  Created by Saadat Abbas on 11/17/24.
 //
 
-struct Pins{
-   
-}
+import Foundation
 
-struct Events: Identifiable{
-   var id: String?
+struct Pins: Identifiable, Hashable{
+   var id: Int?
    let name: String
+   let details: String
    let latitude: Double
    let longitude: Double
    let attendees: Int
    let Public: Bool
+}
+
+struct Events: Identifiable{
+   let id = UUID()
+   var eventPins = [Pins]()
 }
